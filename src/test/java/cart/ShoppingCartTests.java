9059db78d919c5b9c05a74715c83502e32634a92
Bogdan640 <bogdanbubu2004@gmail.com>
2025-08-07 14:41:53 +0300

    dropdown fill attributes methods work for now

--- src/test/java/cart/ShoppingCartTests.java
+++ src/test/java/cart/ShoppingCartTests.java
@@ -1,6 +1,7 @@
 package cart;
 
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.SelectOption;
 import init.Init;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.options.AriaRole;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
@@ -35,7 +37,7 @@ public class ShoppingCartTests extends Init {
         assertThat(addToCartButton).isEnabled();
         addToCartButton.click();
 
-        System.out.println("Page title after add to cart: " + page.title());
+        //System.out.println("Page title after add to cart: " + page.title());
     }
 
     @Test
@@ -63,20 +65,20 @@ public class ShoppingCartTests extends Init {
                 new Page.GetByRoleOptions().setName("Cool Stuff"));
         assertThat(coolStuffButton).isVisible();
         coolStuffButton.click();
-        System.out.println("Page title after cool stuff button click: " + page.title());
+        //System.out.println("Page title after cool stuff button click: " + page.title());
 
         addToCartButton = page.getByRole(AriaRole.BUTTON,
                 new Page.GetByRoleOptions().setName("Add to Cart"));
         assertThat(addToCartButton.first()).isVisible();
         addToCartButton.first().click();
-        System.out.println("Page title after add to cart button click: " + page.title());
+       // System.out.println("Page title after add to cart button click: " + page.title());
 
         // Remove first product from cart
         Locator removeButton = page.locator(".first .product-cart-remove a[title='Remove Item']");
         assertThat(removeButton).isVisible();
         removeButton.click();
 
-        System.out.println("Page title after remove from cart: " + page.title());
+       // System.out.println("Page title after remove from cart: " + page.title());
     }
 
     private void navigateToJackieOSunglasses() {
@@ -84,12 +86,12 @@ public class ShoppingCartTests extends Init {
                 new Page.GetByRoleOptions().setName("Sale")).first();
         assertThat(saleButton).isVisible();
         saleButton.click();
-        System.out.println("Page title after sale button click: " + page.title());
+       // System.out.println("Page title after sale button click: " + page.title());
 
         Locator productImage = page.locator("a.product-image[title='Jackie O Round Sunglasses']");
         assertThat(productImage).isVisible();
         productImage.click();
-        System.out.println("Page title after product image click: " + page.title());
+       // System.out.println("Page title after product image click: " + page.title());
     }
 
 
@@ -105,46 +107,100 @@ public class ShoppingCartTests extends Init {
 
         Locator dropdowns = page.locator("#product-options-wrapper dd .required-entry  ");
 
-        System.out.println(dropdowns.count());
+       // System.out.println(dropdowns.count());
 
         if (dropdowns.count() > 0){
-            for (int i = 1; i <= dropdowns.count(); i++){
-                System.out.println(i);
+            for (int i = 0; i < dropdowns.count(); i++){
+               // System.out.println(i);
                 Locator dropdown = dropdowns.nth(i);
                 assertThat(dropdown).isVisible();
                 assertThat(dropdown).isEnabled();
                 dropdown.click();
 
 
+                Locator optionsFromDropdown = dropdown.locator("option");
+
+                List<String> optionList = new ArrayList<>();
+                List<String> valueList = new ArrayList<>();
+
+                for (Locator option : optionsFromDropdown.all()) {
+                    String optionText = option.textContent().trim();
+                    String optionValue = option.getAttribute("value");
+                    String optionClass = option.getAttribute("class");
+
+
+                    if(optionText.matches(".*Choose.* | .*Select.*") ||
+                        optionValue == null || optionValue.isEmpty() ||
+                            (optionClass != null && optionClass.contains("not-available"))) continue;
+
+
+                    optionList.add(optionText);
+                    valueList.add(optionValue);
+                    //System.out.println("Option text: " + optionText + ", Value: " + optionValue);
+                }
+
+
+
+                for (int j = 0; j < optionList.size(); j++){
+                   // System.out.println("Option " + j + " = " + optionList.get(j));
+
+                    dropdown.selectOption(new SelectOption().setValue(valueList.get(j)));
+
+                    Locator outOfStock = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("OUT OF STOCK"));
+                    if (outOfStock.isVisible()) continue;
+                    break;
+                }
+            }
+
+        }
+    }
+
+
+    private void selectOptionalDropdownAttributes(){
+        Locator dropdowns = page.locator("#product-options-wrapper dd select:not(.required-entry) ");
+
+       // System.out.println(dropdowns.count());
+
+        if (dropdowns.count() > 0){
+            for (int i = 0; i < dropdowns.count(); i++){
+               // System.out.println(i);
+                Locator dropdown = dropdowns.nth(i);
+                assertThat(dropdown).isVisible();
+                assertThat(dropdown).isEnabled();
+                dropdown.click();
 
 
                 Locator optionsFromDropdown = dropdown.locator("option");
 
                 List<String> optionList = new ArrayList<>();
+                List<String> valueList = new ArrayList<>();
+
                 for (Locator option : optionsFromDropdown.all()) {
-                    String optionText = option.textContent();
+                    String optionText = option.textContent().trim();
+                    String optionValue = option.getAttribute("value");
+                    String optionClass = option.getAttribute("class");
+
+
+                    if(optionText.matches(".*Choose.* | .*Select.*") ||
+                            optionValue == null || optionValue.isEmpty() ||
+                            (optionClass != null && optionClass.contains("not-available"))) continue;
+
+
                     optionList.add(optionText);
-                    System.out.println(optionText);
+                    valueList.add(optionValue);
+                   // System.out.println("Option text: " + optionText + ", Value: " + optionValue);
                 }
 
-//                Locator options = dropdown.locator("option");
-//                int optionCount = options.count();
+
 
                 for (int j = 0; j < optionList.size(); j++){
-                    System.out.println("Option " + j + " = " + optionList.get(j));
-                    Locator option = page.getByText(optionList.get(j));
-                    System.out.println(option);
+                   // System.out.println("Option " + j + " = " + optionList.get(j));
 
-                    option.click();
+                    dropdown.selectOption(new SelectOption().setValue(valueList.get(j)));
+
                     Locator outOfStock = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("OUT OF STOCK"));
                     if (outOfStock.isVisible()) continue;
-                    Locator addButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add to Cart"));
-                    assertThat(addButton).isVisible();
-                    assertThat(addButton).isEnabled();
-                    addButton.click();
-                    System.out.println("Page title after add to cart button click: " + page.title());
                     break;
-                    //locator selectoption
                 }
             }
 
@@ -153,19 +209,20 @@ public class ShoppingCartTests extends Init {
 
     @Test
     @DisplayName("Should add a product to the cart successfully")
-    void ttestAddProductToCart() {
+    void mockTestToTestMethods() {
 
         page.navigate(HomepageURL);
 
         page.navigate("http://qa3magento.dev.evozon.com/home-decor/electronics/camera-travel-set.html");
 
         selectMandatoryDropdownAttributes();
+        selectOptionalDropdownAttributes();
 
         Locator addToCartButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add to Cart"));
         assertThat(addToCartButton).isVisible();
         assertThat(addToCartButton).isEnabled();
         addToCartButton.click();
-        page.waitForTimeout(5000);
+
 
 
     }
